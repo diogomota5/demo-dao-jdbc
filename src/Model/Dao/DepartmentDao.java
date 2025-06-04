cd668a7a8aa3fa745ee39c7fa89586dfb7a34124
diogomota5 <diogo_mota1990@outlook.com>
2025-06-04 14:45:10 -0300

    DepartmentDao and SellerDao interfaces

---
+++ src/Model/Dao/DepartmentDao.java
@@ -0,0 +1,14 @@
+package Model.Dao;
+
+import java.util.List;
+
+import Model.Entities.Department;
+
+public interface DepartmentDao {
+
+	void insert(Department obj);
+	void update(Department obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+}
---
+++ src/Model/Dao/SellerDao.java
@@ -0,0 +1,14 @@
+package Model.Dao;
+
+import java.util.List;
+
+import Model.Entities.Seller;
+
+public interface SellerDao {
+
+	void insert(Seller obj);
+	void update(Seller obj);
+	void deleteById(Integer id);
+	Seller findById(Integer id);
+	List<Seller> findAll();
+}
